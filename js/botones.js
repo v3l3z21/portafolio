1c4080479b95622d391e8bb56f5a3d1c38ce0881
v3l3z <javr7481@gmail.com>
2025-10-24 13:21:08 -0500

    commit inicial

---
+++ js/botones.js
@@ -0,0 +1,32 @@
+function redireccionrA(redsocial) {
+    if (redsocial === 'WP') {
+        window.open("https://wa.me/qr/KBEBB5WURXGYM1" , "_blank");
+
+    }
+    if (redsocial === 'IG') {
+        window.open("https://www.instagram.com/v3l3z_21", "_blank");
+
+    }
+    if (redsocial === 'TT') {
+        window.open("https://www.tiktok.com/@v3l3z_21?_t=ZS-90kI2secV2A&_r=1", "_blank");
+
+    }
+}
+
+function redireccionH(inicio) {
+    if (inicio === 'mi') {
+        window.location.href = "acercademi.html";
+    }
+    if (inicio === 'xp') {
+        window.location.href = "experiencia.html";
+        
+    }
+    if (inicio === 'cont') {
+        window.location.href = "contacto.html";
+        
+    }
+    if (inicio === 'pri') {
+        window.location.href = "principal.html";
+        
+    }
+}
